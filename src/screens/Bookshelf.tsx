fd29cab9d95da42607f09252334dba72b74d8216
Andre George <andregeorge@Andres-MacBook-Pro.local>
2024-05-01 15:32:09 +0100

    organise bookshelf screen layout

--- src/screens/Bookshelf.tsx
+++ src/screens/Bookshelf.tsx
@@ -1,19 +1,83 @@
-import {View, Text} from 'react-native';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
 import {Button as RNButton} from '../components/Button';
+import { themePrimary } from '../theme';
+import { FlatList } from 'react-native-gesture-handler';
 
 export const Bookshelf = () => (
-  <View style={{justifyContent: 'center', marginTop: 400}}>
-    <Text>Bookshelf</Text>
-    <View style={{ flexDirection: 'row', justifyContent: 'space-around'}}>
+  // Sectionlist
+  // header: sort view
+  // body: list
+  // footer: buttons
+  <>
+  <View style={{alignItems: 'center', marginTop: 5}}>
+    <Text style={{color: 'black'}}>Sort</Text>
+  </View>
+  <View style={{justifyContent: 'flex-end', alignItems: 'baseline'}}>
+    {/* <View style={{ flexDirection: 'row', justifyContent: 'space-around'}}>
       <Button title="Add Book" />
       <Button title="Edit Book" />
       <Button title="Remove Book" />
+    </View> */}
+    <View style={styles.listItem}>
+      <View style={{flex: 2}}>
+        <Text style={styles.listFont}>Title: </Text>
+        <Text style={styles.listFont}>Author: </Text>
+        <Text style={styles.listFont}>Genre: </Text>
+        <Text style={styles.listFont}>Status: </Text>
+      </View>
+      <View style={{width: 100, height: 100, backgroundColor: 'black', flex: 1, justifyContent: 'center', alignItems: 'flex-end'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'white'}}></View>
+      </View>
+    </View>
+    <View style={styles.listItem}>
+      <View style={{flex: 2}}>
+        <Text style={styles.listFont}>1</Text>
+        <Text style={styles.listFont}>1</Text>
+        <Text style={styles.listFont}>1</Text>
+      </View>
+      <View style={{width: 100, height: 100, backgroundColor: 'black', flex: 1, justifyContent: 'center', alignItems: 'flex-end'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'white'}}></View>
+      </View>
+    </View>
+
+
+
+    <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+      <StackBTN title='Add Book' />
+      <StackBTN title='Edit Book' />
+      <StackBTN title='Remove Book' />
     </View>
   </View>
+  </>
 );
 
 const Button = ({title}) => (
-  <View style={{borderRadius: 25, overflow: 'hidden', flex: 1}}>
+  <View style={{borderRadius: 25, overflow: 'hidden', flex: 1, backgroundColor: 'blue'}}>
     <RNButton title={title} />
   </View>
 );
+
+const StackBTN = ({title}: {title: string}) => (
+  <Pressable style={styles.stackBtn}>
+    <Text style={{color: 'white', fontSize: 14}}>{title}</Text>
+  </Pressable>
+);
+
+const styles = StyleSheet.create({
+  stackBtn: {
+    height: 40,
+    width: 10,
+    borderRadius: 25,
+    flex: 1,
+    backgroundColor: themePrimary,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listItem: {
+    padding: 10,
+    flexDirection: 'row'
+  },
+  listFont: {
+    fontSize: 20,
+  }
+});
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -22,18 +22,6 @@ export const Home = () => (
   </>
 );
 
-const Cover = () => (
-  <>
-    <Image
-      style={{width: '100%', height: '100%'}}
-      source={{
-        uri: 'https://d1uvxqwmcz8fl1.cloudfront.net/tes/resources/6441170/45b3db43-ab3d-42a4-9a8a-5053d8fd5013/image?width=500&height=500&version=1474643904786',
-      }}
-    />
-  </>
-);
-
-
 
 const styles = StyleSheet.create({
   bookmarkScene: {
