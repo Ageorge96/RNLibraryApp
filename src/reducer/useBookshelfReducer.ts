aeb1ebffc4ef907bb9d4207e05e465c35423f451
Andre George <andregeorge@Andres-MacBook-Pro.local>
2024-05-08 12:31:19 +0100

    add bookshelf reducer hook, change status types to string | undefined, change SectionList sections to use hook return

--- src/reducer/useBookshelfReducer.ts
+++ src/reducer/useBookshelfReducer.ts
@@ -1,7 +1,21 @@
+import {completeBookMockData} from '@src/data/MockData';
+import {CompleteBook} from '@src/data/types';
+import {BookshelfBook, BookshelfSectionListProps} from '@src/scenes/types';
 
-
-export const useBookshelfReducer = () => {
-  const transformedBookshelfData = {}
-  
-  return transformedBookshelfData;
+export const useBookshelfReducer = (/*user's book list*/) => {
+  const transformedBookshelfData: BookshelfSectionListProps = [
+    {
+      title: 'Bookshelf',
+      data: completeBookMockData.map(
+        (book: CompleteBook): BookshelfBook => ({
+          title: book.title,
+          author: book.author,
+          genre: book.genre,
+          status: book.status,
+          cover: book.cover,
+        }),
+      ),
+    },
+  ];
+  return {transformedBookshelfData};
 };
