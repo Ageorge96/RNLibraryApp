4f2f82d4733a16f8abed54e010840665a96bdf4f
Andre George <andregeorge@Andres-MacBook-Pro.local>
2024-05-15 11:00:11 +0100

    add .env variable, add env config to ios and android build

--- index.js
+++ index.js
@@ -4,9 +4,13 @@
 import {AppRegistry} from 'react-native';
 import {App} from './src/App';
 import {name as appName} from './app.json';
+import Config from 'react-native-config';
 
-// let AppEntryPoint = App;
+let AppEntryPoint = App;
 
-const AppEntryPoint = require('./.storybook').default;
+console.log(Config.ENABLE_STORYBOOK);
+if (Config.ENABLE_STORYBOOK === 'true') {
+  AppEntryPoint = require('./.storybook').default;
+}
 
 AppRegistry.registerComponent(appName, () => AppEntryPoint);
