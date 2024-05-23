a8fa2f870845768e21f97c1c25e03b91d03a9cd7
Andre George <andregeorge@Andres-MacBook-Pro.local>
2024-05-23 17:30:11 +0100

    add footer to Bookshelf

--- src/feature/BookshelfSectionList/BookshelfSectionList.tsx
+++ src/feature/BookshelfSectionList/BookshelfSectionList.tsx
@@ -2,7 +2,7 @@ import {StackBTN} from '@components/StackBTN';
 import {BookshelfListItem} from '@components/scenes/bookshelfListItem/BookshelfListItem';
 import {useBookshelfReducer} from '@src/reducer/useBookshelfReducer';
 import {themePrimary} from '@src/theme';
-import {SectionList, View, Text, StyleSheet} from 'react-native';
+import {SectionList, View, Text, StyleSheet, Pressable} from 'react-native';
 import { BookshelfHeader } from './BookshelfHeader';
 import { themeSecondary } from '@src/theme/Theme';
 
@@ -36,22 +36,35 @@ export const BookshelfSectionList = () => {
         )}
         stickyHeaderHiddenOnScroll
       />
-      <View style={{width: '100%', height: 60, backgroundColor: themePrimary, flexDirection: 'row', justifyContent: 'space-evenly'}}>
-        <View style={[styles.barView, {borderEndWidth: 5}]}>
+      <View
+        style={{
+          width: '100%',
+          height: 60,
+          backgroundColor: themePrimary,
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+        }}>
+        <Pressable style={[styles.barView, {borderEndWidth: 2}]} onPress={() => {console.log('1')}}>
           <Text style={styles.bar}>Add Book</Text>
-        </View>
-        <View style={styles.barView} >
+        </Pressable>
+        <Pressable style={styles.barView} onPress={() => {console.log('2')}}>
           <Text style={styles.bar}>Edit Book</Text>
-        </View>
-        <View style={[styles.barView, {borderStartWidth: 5}]}>
+        </Pressable>
+        <Pressable style={[styles.barView, {borderStartWidth: 2}]} onPress={() => {console.log('3')}}>
           <Text style={styles.bar}>Remove Book</Text>
-        </View>
+        </Pressable>
       </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  bar: {color: themeSecondary, marginHorizontal: 'auto', justifyContent: 'center', alignItems: 'center'},
-  barView: {borderColor: themeSecondary, flex: 1, paddingTop: 5}
-})
+  bar: {
+    color: themeSecondary,
+    fontSize: 18,
+    marginHorizontal: 'auto',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  barView: {borderColor: themeSecondary, flex: 1, paddingTop: 5},
+});
