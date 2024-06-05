95b340d38e66aa8e02e32d47a3d3fb413fddfa87
Andre George <andregeorge@Andres-MacBook-Pro.local>
2024-06-05 10:59:14 +0100

    add basic carousel to Home Screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,13 @@
-import {View} from 'react-native';
+import {Dimensions, View} from 'react-native';
 import {bookmarkData, wishlistData} from '@src/data/MockData';
 import {styles} from './styles';
 import {HomeScreenFlatList} from '@src/feature/HomeFlatListSection/HomeScreenFlatList';
 import {Text} from '@components/Text';
+import Carousel from 'react-native-reanimated-carousel';
+import { faker } from '@faker-js/faker';
+import { V } from '@storybook/react-native/dist/View-9ba91d66';
+import { Cover } from '@components/index';
+import { themeSecondary } from '@src/theme/Theme';
 
 // The same data should be able to be passed in without edit, adjustments to data should be handled within component
 export const Home = () => (
@@ -11,9 +16,30 @@ export const Home = () => (
       <Text style={{fontSize: 24, color: 'black'}}>My Bookmarks</Text>
     </View>
     <HomeScreenFlatList data={bookmarkData} />
-    <View style={styles.homeTitleView}>
+    <View style={[styles.homeTitleView, {marginTop: 5}]}>
       <Text style={styles.homeTextText}>Wishlist</Text>
     </View>
     <HomeScreenFlatList data={wishlistData} />
+    <View style={{flex: 1, marginTop: 5}}>
+      <Carousel 
+        loop
+        width={Dimensions.get('window').width}
+        height={Dimensions.get('window').height / 3}
+        autoPlay
+        style={{flex: 1, justifyContent: 'center'}}
+        
+        data={covers}
+        renderItem={({index}) => (
+          <View style={{alignItems: 'center', justifyContent: 'center'}}>
+            <View style={{width: '30%', height: '90%', borderWidth: 1, borderRadius: 25, overflow: 'hidden',}}>
+              <Cover cover={covers[index]} title={`${index}`} />
+            </View>
+          </View>
+        )}
+      />
+    </View>
   </>
 );
+
+
+const covers = [faker.image.url(), faker.image.url(), faker.image.url(), faker.image.url(),]
--- src/screens/styles.ts
+++ src/screens/styles.ts
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-  homeTitleView: {marginTop: 15, marginLeft: 20},
+  homeTitleView: {marginTop: 10, marginLeft: 20},
   homeTextText: {fontSize: 24, color: 'black'},
 });
