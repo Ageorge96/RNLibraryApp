b89abcec85dee42ab78b73409474441df9a15914
Andre George <andregeorge@Andres-MacBook-Pro.local>
2024-06-26 14:44:40 +0100

    add useBookshelfReducer doc

---
+++ docs/useBookshelfReducer.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Custom hook to reduce book data to data necessary for bookshelf
+ *
+ * @param data - complete list of the users's book data
+ *
+ * @returns transformedBookshelfData - returns a convert list of book data
+ *
+ */
